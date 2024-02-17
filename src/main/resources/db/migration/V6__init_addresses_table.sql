305de9cf8b16891c6a18893e16b017bb7a494f0c
Safarov Renat <SafarovRenat1995@gmail.com>
2024-02-17 09:36:00 +0200

    changed insert into "addresses"

--- src/main/resources/db/migration/V6__init_addresses_table.sql
+++ src/main/resources/db/migration/V6__init_addresses_table.sql
@@ -1,14 +1,13 @@
 INSERT INTO addresses
 (city, street, number, zip, apartment, person_id)
 values
-    ('city 1', 'street 1', 1, 'zip 1', 1001, 1),
-    ('city 2', 'street 2', 2, 'zip 2', 2002, 2),
-    ('city 3', 'street 3', 3, 'zip 3', 3003, 3),
-    ('city 4', 'street 4', 4, 'zip 4', 4004, 4),
-    ('city 5', 'street 5', 5, 'zip 5', 5005, 5),
-    ('city 6', 'street 6', 6, 'zip 6', 6006, 6),
-    ('city 7', 'street 7', 7, 'zip 7', 7007, 7),
-    ('city 8', 'street 8', 8, 'zip 8', 8008, 8),
-    ('city 9', 'street 9', 9, 'zip 9', 9009, 9),
-    ('city 10', 'street 10', 10, 'zip 10', 1010, 10);
-
+    ('city 1', 'street 1', 1, 1001, 'apt 1', 1),
+    ('city 2', 'street 2', 2,  2002, 'apt 2', 2),
+    ('city 3', 'street 3', 3, 3003, 'apt 3', 3),
+    ('city 4', 'street 4', 4, 4004, 'apt 4', 4),
+    ('city 5', 'street 5', 5, 5005, 'apt 5', 5),
+    ('city 6', 'street 6', 6, 6006, 'apt 6', 6),
+    ('city 7', 'street 7', 7, 7007, 'apt 7', 7),
+    ('city 8', 'street 8', 8, 8008, 'apt 8', 8),
+    ('city 9', 'street 9', 9, 9009, 'apt 9', 9),
+    ('city 10', 'street 10', 10, 1010,'apt 10',  10);
